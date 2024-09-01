725a8992bb140e9a86ebdea7d486e13c229b5406
Harsh Saw <harshsaw2607@.gmail.com>
2024-09-01 13:09:20 +0530

    Added mimddleware for Rate limiting

--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,31 @@
 import express from 'express'
+import cors from 'cors'
 const app=express();
 const PORT=3002;
 import { DBConnection } from './database/db.js';
 import UrlRouter from './Router/UrlRouter.js';
+import { rateLimiter } from './RateLimiter.js';
+import AuthRouter from './Router/AuthRouter.js';
+import cookieParser from 'cookie-parser';
 DBConnection();
 
+app.use(cors({ origin:"http://localhost:5173", 
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization'
+ }))
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(rateLimiter);
 
 app.get('/',(req,res)=>{
     res.send('Hello');
 })
 
-app.use('/api/',UrlRouter);
+app.use('/api/HandleURL/',UrlRouter);
+app.use('/api/auth/', AuthRouter)
 
 
 
